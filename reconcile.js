75a2fa99098282149007455874f7626c57572b11
bradsharp <bradleysharpie@gmail.com>
2019-06-17 11:31:57 +0100

    Converted reconciler to use promises, also gets latest dump online

--- reconcile.js
+++ reconcile.js
@@ -1,42 +1,68 @@
 const fs = require('fs');
-const childProcess = require('child_process');
+const request = require('request');
 
-childProcess.exec("RobloxStudioBeta.exe -API dump.json", (err, stdout, stderr) => {
-	if (err)
-		throw err;
-	
-});
-
-function write(name, data) {
-	fs.writeFile(`${name}.json`, JSON.stringify(data, null, "	"), 'utf8', (err) => {
-		if (err)
-			console.error(err);
+let fetchLatestDump = () => {
+	return new Promise((resolve, reject) => {
+		request('http://setup.roblox.com/versionQTStudio', (err, res, body) => {
+			if (err)
+				reject(err);
+			else
+				request(`http://setup.roblox.com/${body}-API-Dump.json`, {json: true}, (err, res, body) => {
+					if (err)
+						reject(err);
+					else
+						resolve(body);
+				});
+		});
 	});
 }
 
-let instances = {};
-let classes = input.Classes;
-
-var reads = 0;
-
-classes.forEach(instance => {
-	let name = instance.Name
-	reads++;
-	fs.readFile(`Instances/${name}/Description.md`, "utf8", (err, data) => {
-		console.log(name, err, data)
-		reads--;
-		instances[name] = {
-			// Members: instance.Members,
-			Super: instance.Superclass,
-			Tags: instance.Tags,
-			ShortDescription: "Undocumented",
-			Description: data == null ? "" : data,
-			Related: [],
-		}
-		if (reads == 0)
-			write("instances", instances);
+let readFile = (fileName) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(fileName, "utf8", (err, data) => {
+			if (err)
+				reject(err);
+			else
+				resolve(data);
+		});
 	});
-	
-});
+}
 
-write("enums", input.Enums);
+let asyncForEach = async (array, callback) => {
+	for (let index = 0; index < array.length; index++)
+		await callback(array[index], index, array);
+}
+
+let run = async () => {
+	let classes = {};
+	let dump = await fetchLatestDump();
+
+	await asyncForEach(dump.Classes, async (element) => {
+		let instance = {
+			// Members: instance.Members,
+			Super: element.Superclass,
+			Tags: element.Tags,
+			ShortDescription: "Undocumented",
+			Description: "",
+			Related: []
+		}
+		try {
+			instance.Description = await readFile(`docs/Instances/${element.Name}/${element.Name}.md`);
+		}
+		catch {
+		}
+		classes[element.Name] = instance;
+	})
+
+	let apiRef = {
+		classes: classes,
+		dataTypes: null
+	}
+
+	fs.writeFile(`api.json`, JSON.stringify(apiRef, null, "	"), 'utf8', (err) => {
+		if (err)
+			throw err;
+	});
+}
+
+run();
