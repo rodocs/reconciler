209df9bf0fd0015c8507dc0fdecdfe64da6c8216
bradsharp <bradleysharpie@gmail.com>
2019-06-18 13:01:25 +0100

    Switched to improve front matter handling

--- reconcile.js
+++ reconcile.js
@@ -18,24 +18,21 @@ let fetchLatestDump = () => {
 	});
 }
 
-let readFile = (fileName) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(fileName, "utf8", (err, data) => {
-			if (err)
-				reject(err);
-			else
-				resolve(data);
-		});
-	});
+let formatDocumentation = (data) => {
+	var documentation = fm(data);
+	return {
+		Attributes:	documentation.attributes,
+		Content:	documentation.body
+	}
 }
 
 let getDocumentation = (folder, name) => {
 	return new Promise((resolve, reject) => {
 		fs.readFile(`${folder}/${name}.md`, "utf8", (err, data) => {
-			if (err) // TODO: Only resolve to null when no file found
+			if (err) // TODO: Only resolve to undefined when no file found
 				resolve(undefined);
 			else
-				resolve(fm(data));
+				resolve(formatDocumentation(data));
 		});
 	});
 }
